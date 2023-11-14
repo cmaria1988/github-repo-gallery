54ff6db43ccc4a5af097799492e8f4da184acc15
Maria <claudmaria1988@gmail.com>
2023-11-14 11:05:29 +0800

    changing in javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const overview = document.querySelector(".overview");
 const username = "cmaria1988";
+const repoList = document.querySelector(".repo-list");
 
 const githubProfile = async function(){
     const userInfo = await fetch(`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
     displayUserInfo(data);
 }
-
 githubProfile();
 
 const displayUserInfo = function(data){
@@ -23,4 +23,21 @@ const displayUserInfo = function(data){
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div> `;
     overview.append(userInfoDiv);
+    githubRepos();
 }
+
+const githubRepos = async function(){
+    const repoData = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repos = await repoData.json();
+    displayRepositories(repos);
+}
+
+const displayRepositories = function(repos){
+    for(const repo of repos){
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+}
+ 
